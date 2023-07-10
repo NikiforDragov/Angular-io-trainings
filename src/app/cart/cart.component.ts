96d957cb9a483e5b15827ef9e3a9cc248d3afccd
NikiforDragov <114018028+NikiforDragov@users.noreply.github.com>
2023-07-10 18:53:37 +0300

    Add purchase functionality

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
+
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {
+  checkoutForm = this.formbuilder.group({
+    name: '',
+    adress: '',
+  });
 
+  constructor(
+    private cartService: CartService,
+    private formbuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset()
   }
 }
